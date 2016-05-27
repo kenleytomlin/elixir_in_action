1e30c5ca4e27802aacd9d3aaab9e7ea62d893b1a
kenleytomlin <kenleytomlin@gmail.com>
2016-05-27 17:17:01 +0900

    Change module name in test

--- test/streams_enum_test.exs
+++ test/streams_enum_test.exs
@@ -2,22 +2,22 @@ defmodule StreamsEnumTest do
   use ExUnit.Case
 
   test "large_lines_num! returns the number of lines from a file that are longer than 80 characters" do
-    assert ElixirInAction.large_lines_num!("./test/fixtures/test_file.txt") == 5
+    assert StreamsEnum.large_lines_num!("./test/fixtures/test_file.txt") == 5
   end
 
   test "lines_lengths! takes a file path and returns a list of numbers with each number representing the length of the correspinding line from the file" do
-    assert ElixirInAction.lines_lengths!("./test/fixtures/lines_length.txt") == [5,2,1]
+    assert StreamsEnum.lines_lengths!("./test/fixtures/lines_length.txt") == [5,2,1]
   end
 
   test "longest_line_length! returns the length of the longest line" do
-    assert ElixirInAction.longest_line_length!("./test/fixtures/lines_length.txt") === 5
+    assert StreamsEnum.longest_line_length!("./test/fixtures/lines_length.txt") === 5
   end
 
   test "longest_line! returns the contents of the longest line in a file" do
-    assert ElixirInAction.longest_line!("./test/fixtures/lines_length.txt") === "ABCDE"
+    assert StreamsEnum.longest_line!("./test/fixtures/lines_length.txt") === "ABCDE"
   end
 
   test "words_per_line! returns a list of numbers with each number representing the word count of a line" do
-    assert ElixirInAction.words_per_line!("./test/fixtures/words_per_line.txt") === [3,2,1]
+    assert StreamsEnum.words_per_line!("./test/fixtures/words_per_line.txt") === [3,2,1]
   end
 end
